e3c46e731ff5d05893dddff119a43672bdd02dba
conceit-sz <bullnick20@gmail.com>
2020-05-09 16:59:13 +0800

    准备分支

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ const Index = {
   init() {
     this.$start = $("button.start")
     this.$speech = $("div.speech")
-    this.doSomething = doSomething
+    this.$$modules = $$("div.module")
     
     this.bind()
   },
@@ -29,9 +29,12 @@ const Index = {
       ]
     }).then(result => {
       if(result.filePaths.length !== 0) {
+
+
         let pathName = result.filePaths[0]
-        this.doSomething(pathName)
+        CoreCalc.init(pathName)
         this.$speech.classList.add("hidden")
+        this.$$modules.forEach($module => $module.classList.add("appear"))
       }
     }).catch(error => {
       console.log(error)
@@ -40,6 +43,26 @@ const Index = {
       this.$speech.classList.remove("clicked")
     })
   }
+}
+
+const CoreCalc = {
+  init(pathName) {
+    console.log("CoreCalc init..")
+    this.doSomething = doSomething
+    this.options = {}
+    this.options2 = []
+
+    this.start(pathName)
+  },
+
+  start(m) {
+    this.options = this.doSomething(m)
+  },
+
+  render() {
+
+  }
+
 }
 
 const App = {
